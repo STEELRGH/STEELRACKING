099866d2aa489149ab468dd2fdccee02895b2d84
STEELRGH <steelracking@myodoo.sysasoluciones.com>
2024-10-28 13:18:17 -0600

    Delete cfdi_boveda_fiscal directory

--- cfdi_boveda_fiscal/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import cfdi_download_request
-from . import cfdi_dowload_fiel
-from . import cfdi_dowload_pack
-from . import cfdi_download_data
-from . import account_move
