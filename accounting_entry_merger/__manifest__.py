ecd98e3f0105515f184aa08a3be110629ca75ba9
STEELRGH <steelracking@myodoo.sysasoluciones.com>
2024-10-28 18:37:23 -0600

    Add files via upload

---
+++ accounting_entry_merger/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": 'Accounting Entry Merger',
+    'license': 'GPL-3',
+    "version": '17.1',
+    "author": 'DuvanDevs',
+    "category": '',
+    "website": "",
+    "description": """Permite visualizar las lineas de asientos y apuntes""",
+    "depends": ['base', 'account_accountant'],
+    "data": [
+        'views/account_move_view.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
