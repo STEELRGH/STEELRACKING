adcdc908ae4b5a329b5c8f8bf57fbf3318130d4d
JosueFO <josueflores.05@gmail.com>
2025-02-06 21:58:02 -0600

    [FIX] report payment receipt document

--- sb_custom_reports/__manifest__.py
+++ sb_custom_reports/__manifest__.py
@@ -24,13 +24,13 @@ Long description of module's purpose
     'data': [
         # 'security/ir.model.access.csv',
         # 'reports/report_invoice_document.xml',
+        # 'reports/action_report.xml',
         'reports/external_layout_striped_inherit.xml',
         'reports/document_tax_totals_template_extend.xml',
         'reports/tax_groups_totals_extend.xml',
         'reports/report_invoice_document_inherit.xml',
         'reports/report_payment_receipt_document_inherit.xml',
         'reports/report_saleorder_document_inherit.xml',
-        'reports/action_report.xml',
     ],
     'assets': {
         'web.report_assets_common': [
