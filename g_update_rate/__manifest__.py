ddeb4d9a30a581247b3469dad5101829e514c0cd
Gerardo-MiRe <geramr8@outlook.com>
2025-06-14 00:07:54 -0600

    [ADD] welcome module g_update_rate

---
+++ g_update_rate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Update currency daily',
+    'version': '1.0',
+    'category': 'Stock',
+    'summary': 'Update currency daily',
+    'description': 'Update currency daily',
+    'depends': ['base','currency_rate_live',],
+    'data': [],    
+    'installable': True,
+    'application': False,
+}
---
+++ g_update_rate/models/res_company.py
@@ -0,0 +1,27 @@
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+
+    @api.model
+    def run_update_currency(self):
+        """ This method is called from a cron job to update currency rates.
+        """
+        records = self.search([
+            ('currency_next_execution_date', '<=', fields.Date.today()),
+            ('parent_id', '=', False),
+        ])
+        if records:
+            to_update = self.env['res.company']
+            for record in records:
+                if record.currency_interval_unit == 'daily':
+                    next_update = relativedelta
+                elif record.currency_interval_unit == 'weekly':
+                    next_update = relativedelta
+                elif record.currency_interval_unit == 'monthly':
+                    next_update = relativedelta
+                else:
+                    record.currency_next_execution_date = False
+                    continue
+                record.currency_next_execution_date = datetime.date.today()
+                to_update += record
+            to_update.with_context(suppress_errors=True).update_currency_rates()
